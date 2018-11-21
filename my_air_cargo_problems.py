168f6c33c12a092e6108aaa78f5705f3b8b64ec7
alexjuliansmith <alexjuliansmith@gmail.com>
2018-11-21 15:12:08 +0000

    Feature: Implement AirCargoProblem

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -3,7 +3,7 @@ from aimacode.planning import Action
 from aimacode.search import (
     Node, Problem,
 )
-from aimacode.utils import expr
+from aimacode.utils import expr, Expr
 from lp_utils import (
     FluentState, encode_state, decode_state,
 )
@@ -48,29 +48,59 @@ class AirCargoProblem(Problem):
             list of Action objects
         """
 
-        # TODO create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
+        # DONE create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
         # concrete actions definition: specific literal action that does not include variables as with the schema
         # for example, the action schema 'Load(c, p, a)' can represent the concrete actions 'Load(C1, P1, SFO)'
         # or 'Load(C2, P2, JFK)'.  The actions for the planning problem must be concrete because the problems in
         # forward search and Planning Graphs must use Propositional Logic
+
+        from itertools import product
+        def generate_concrete_actions(schema: Action, *args: iter):
+            '''
+            Helper method to generate concrete Actions from Action Schemas.
+
+            :param schema: Action schema to generate concrete actions for
+            :param args: List of lists of possible argument values for each variable in action schema, presented in same
+            order as variables are introduced in the Action schema.
+
+            :return: List of concrete actions for all possible unique combinations of arguments.
+            '''
+            args = [a for a in list(product(*args)) if len(a) == len(set(a))]
+            concrete_actions = []
+            for arg in args:
+                ground_args = list(map(expr, arg))
+                action = schema.substitute(Expr(schema.name, *schema.args), ground_args)
+                precond_pos = [schema.substitute(precond, ground_args) for precond in schema.precond_pos]
+                precond_neg = [schema.substitute(precond, ground_args) for precond in schema.precond_neg]
+                effect_add = [schema.substitute(effect, ground_args) for effect in schema.effect_add]
+                effect_rem = [schema.substitute(effect, ground_args) for effect in schema.effect_rem]
+                concrete_actions.append(Action(action, [precond_pos, precond_neg], [effect_add, effect_rem]))
+            return concrete_actions
 
         def load_actions():
             """Create all concrete Load actions and return a list
 
             :return: list of Action objects
             """
-            loads = []
-            # TODO create all load ground actions from the domain Load action
-            return loads
+            # DONE create all load ground actions from the domain Load action
+            load_schema = Action(expr("Load(c, p, a)"),
+                                 [[expr("At(c, a)"), expr("At(p, a)")], []],    # Preconditions
+                                 [[expr("In(c, p)")],                           # Add Effects
+                                  [expr("At(c, a)")]])                          # Remove Effects
+
+            return generate_concrete_actions(load_schema, self.cargos, self.planes, self.airports)
 
         def unload_actions():
             """Create all concrete Unload actions and return a list
 
             :return: list of Action objects
             """
-            unloads = []
-            # TODO create all Unload ground actions from the domain Unload action
-            return unloads
+            # DONE create all Unload ground actions from the domain Unload action
+            unload_schema = Action(expr("Unload(c, p, a)"),
+                                   [[expr("In(c, p)"), expr("At(p, a)")], []],  # Preconditions
+                                   [[expr("At(c, a)")],                         # Add Effects
+                                    [expr("In(c, p)")]])                        # Remove Effects
+            return generate_concrete_actions(unload_schema, self.cargos, self.planes, self.airports)
 
         def fly_actions():
             """Create all concrete Fly actions and return a list
@@ -95,6 +125,8 @@ class AirCargoProblem(Problem):
 
         return load_actions() + unload_actions() + fly_actions()
 
+
+
     def actions(self, state: str) -> list:
         """ Return the actions that can be executed in the given state.
 
@@ -103,9 +135,13 @@ class AirCargoProblem(Problem):
             e.g. 'FTTTFF'
         :return: list of Action objects
         """
-        # TODO implement
-        possible_actions = []
-        return possible_actions
+        # DONE implement
+
+
+        # TODO make decode_state more efficient
+        model = decode_state(state, self.state_map)
+        return [action for action in self.actions_list if action_possible(action, model)]
+
 
     def result(self, state: str, action: Action):
         """ Return the state that results from executing the given
@@ -116,9 +152,17 @@ class AirCargoProblem(Problem):
         :param action: Action applied
         :return: resulting state after action
         """
-        # TODO implement
-        new_state = FluentState([], [])
-        return encode_state(new_state, self.state_map)
+        # DONE implement
+        assert action_possible(action, decode_state(state, self.state_map)), "Action must be one of self.actions(state)"
+
+        new_state = list(state)
+        for idx, literal in enumerate(self.state_map):
+            if literal in action.effect_add:
+                new_state[idx] = 'T'
+            elif literal in action.effect_rem:
+                new_state[idx] = 'F'
+
+        return ''.join(new_state)
 
     def goal_test(self, state: str) -> bool:
         """ Test the state to see if goal is reached
@@ -157,9 +201,26 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
-        count = 0
+        # DONE implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+        #  Because the goals in all our problems are conjunctions of clauses of the form:
+        #  At(cargo, airport)
+        #  And because the only action - Unload(cargo, airport) - able to add such a goal literal to the current state:
+        #  1) Cannot remove any goal literal from the state (as the unload action never removes an At literal)
+        #  2) Only adds at most a single At literal to the current state
+        #  We are justified in making the independent subgoal assumption and so the ignore_preconditions heuristic is
+        #  simply the current number of unsatisfied goal conditions.  (as per Russell-Norvig Ed-3 10.2.3 TODO check this)
+        #
+        #  (i.e. the minimum number of actions required is specifically the minimum number of unload actions required).
+
+        model = decode_state(node.state, self.state_map).pos
+        count = len([literal for literal in self.goal if literal not in model])
         return count
+
+def action_possible(action, model):
+    # An action is possible in a state if all the action's preconditions are met by the state
+    return all(precond in model.pos for precond in action.precond_pos) and \
+           all(precond in model.neg for precond in action.precond_neg)  # 2nd clause not strictly necessary
+                                                                        # as ACP actions don't have neg preconds
 
 
 def air_cargo_p1() -> AirCargoProblem:
